832432375e957a884465a23e90746373cfef45ab
Mathis Rosset <rosset.mathis@gmail.com>
2023-04-11 18:11:49 +0200

    evolution script

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 
-console.log('Start');
 
 
-document.querySelector('#departure').addEventListener('click',
- function (){
-   console.log('Click detected!');
- }
-);
 
 
-console.log('End');
+document.querySelector("#btn-search").addEventListener("click", function () {
+  console.log("Click Search detected!");
+
+  const departure = document.querySelector("#departure").value;
+  const arrival = document.querySelector("#arrival").value;
+
+  
+
+});
