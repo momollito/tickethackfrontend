ab9bb824e9803a6c0374d015186b73ddd0777b9b
Mathis Rosset <rosset.mathis@gmail.com>
2023-04-12 10:08:14 +0200

    modification scrip

--- script.js
+++ script.js
@@ -3,12 +3,21 @@
 
 
 
-document.querySelector("#btn-search").addEventListener("click", function () {
+document.querySelector("#btn-search").addEventListener("click", async function () {
   console.log("Click Search detected!");
 
   const departure = document.querySelector("#departure").value;
   const arrival = document.querySelector("#arrival").value;
+  const date = document.querySelector('#calendar').value;
 
-  
+  console.log(departure, arrival, date)
+
+  const rowres = await fetch('http://localhost:3000/trips', {
+    method: "POST",
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({departure, arrival, date})
+  })
+  const resjson = await rowres.json()
+  console.log('fetch res:', resjson)
 
 });
