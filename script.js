90a429ae04a002f6db78e047cecd29062ca0504f
jammealexandre28 <jammealexandre1@gmail.com>
2023-04-12 10:59:18 +0200

    script defined

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ document.querySelector('#departure').addEventListener('click',
 
 console.log('End');
 
+
+document.querySelector('#btn-search').addEventListener('click', 
+      function () {
+        let departCompare = document.getElementById('departure').value;
+        let arrivalCompare = document.getElementById('arrival').value;
+
+
+        console.log(departCompare, arrivalCompare)
+      })
