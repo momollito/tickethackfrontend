6fddcec19707d1ed8d2ec94ae9069a67f0041c1c
Mathis Rosset <rosset.mathis@gmail.com>
2023-04-12 18:00:15 +0200

    fin jeudi

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function missingData() {
   document.querySelector("#cards_1").innerHTML = `
-<div class="columns">
+  <div class="columns" id="result">
 <img class="img_card" src="./images/notfound.png" />
 <div id="separator"></div>
 <p id="subscribe_text">Missing data, please fill the form correctly.</p>
@@ -9,12 +9,17 @@ function missingData() {
 
 function noTrips() {
   document.querySelector("#cards_1").innerHTML = `
-          <div class="columns">
+  <div class="columns" id="result">
           <img class="img_card" src="./images/notfound.png" />
           <div id="separator"></div>
           <p id="subscribe_text">No trips were found, you can walk to your destination.</p>
           </div>`;
 }
+
+//Array with trips to export to cart.html
+// let tripAdded = [];
+
+//Search for Trains
 
 document.querySelector("#btn-search").addEventListener("click", function () {
   console.log("Click Search detected!");
@@ -23,7 +28,9 @@ document.querySelector("#btn-search").addEventListener("click", function () {
   const arrival = document.querySelector("#arrival").value;
   const rowdate = document.querySelector("#calendar").value;
   const date = new Date(rowdate).toLocaleDateString("en-GB");
+  //console.log('Data requested =>', departure, arrival,date)
 
+  //Fetch trips from BackEnd
   const fetchTrips = async () => {
     const rowres = await fetch("http://localhost:3000/trips", {
       method: "POST",
@@ -31,44 +38,41 @@ document.querySelector("#btn-search").addEventListener("click", function () {
       body: JSON.stringify({ departure, arrival, date }),
     });
     const resjson = await rowres.json();
-    console.log("response", resjson);
+    document.querySelector("#result").remove();
+
     if (resjson["trips"] === "Missing data") {
       missingData();
     } else if (resjson["trips"] === "No trips were found") {
-      noTrips()
+      noTrips();
     } else {
-      document.querySelector("#train").remove();
+
+      //ADD trips in front page for selection
+      document.querySelector("#cards_1").innerHTML = `
+        <div class="columns" id="result"></div>`;
 
       for (trip of resjson["trips"]) {
         let departureRes = trip.departure;
         let arrivalRes = trip.arrival;
         let hourRes = trip.date;
         let priceRes = trip.price;
-        console.log(departureRes, arrivalRes, hourRes);
-
+        console.log('Data from BD =>', departureRes, arrivalRes,hourRes,priceRes)
         document.querySelector("#result").innerHTML += `
         <div class="row">
         <div class="text_container">
             <p>${departureRes} 🔜 ${arrivalRes}</p>
             <p>${hourRes}</p>
             <p>${priceRes}€</p>
-            <span class="delete">Add</span>
+            <span class="add">Add</span>
         </div>
+      </div>
       </div>`;
+
       }
+
+      trip.addEventListener("click", function () {
+        console.log("Click ADD detected!");
+      });
     }
   };
-
   fetchTrips();
 });
-
-// Journey not found - HMTL
-
-{
-  /* <div class="card_model" id="cards_1">
-<div class="columns_train">
-    <img class="img_card" src="./images/notfound.png" />
-    <div id="separator"></div>
-    <p id="subscribe_text">No trip</p>
-</div> */
-}
